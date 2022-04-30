77a6b88dd5f698b8285afff7d91d18b4594cb3e9
cs536 <meetgajen@outlook.com>
2022-04-29 21:20:34 -0500

    first commit

---
+++ src/main/java/com/spring/jenskins/SpringJenskinsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jenskins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenskinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenskinsApplication.class, args);
+	}
+
+}
